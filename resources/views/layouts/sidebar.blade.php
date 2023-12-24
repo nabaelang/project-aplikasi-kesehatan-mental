480a169f7a13687cf6859c67092e69dd61def64a
reonaldi152 <reo@isysedge-soft.com>
2023-12-24 10:47:35 +0700

    fixing routes

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,14 +2,14 @@
 <aside id="sidebar" class="sidebar">
     <ul class="sidebar-nav" id="sidebar-nav">
         <li class="nav-item">
-            <a class="nav-link " href="/>
+            <a class="nav-link " href="/">
                 <i class="bi bi-grid"></i>
                 <span>Dashboard</span>
             </a>
         </li><!-- End Dashboard Nav -->
 
         <li class="nav-item">
-            <a class="nav-link " href="/>
+            <a class="nav-link " href="/">
                 <i class="bi bi-person"></i>
                 <span>Users</span>
             </a>

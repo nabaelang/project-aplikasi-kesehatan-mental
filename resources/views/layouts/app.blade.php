1197ab7b524c05d26669e8915c2282dfba9dbe94
reonaldi152 <reo@isysedge-soft.com>
2023-12-24 10:45:50 +0700

    fixing cms

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,28 +48,30 @@
 
     @include('layouts.header')
     @include('layouts.sidebar')
-    <div>
+    <main id="main" class="main">
         @yield('content')
-        {{-- @include('sweetalert::alert') --}}
-    </div>
-
-    <!-- Vendor JS Files -->
-    <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
-    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/vendor/chart.js/chart.umd.js"></script>
-    <script src="assets/vendor/echarts/echarts.min.js"></script>
-    <script src="assets/vendor/quill/quill.min.js"></script>
-    <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
-    <script src="assets/vendor/tinymce/tinymce.min.js"></script>
-    <script src="assets/vendor/php-email-form/validate.js"></script>
-
-    <!-- Template Main JS File -->
-    <script src="{{ asset('js/main.js') }}"></script>
-
+    </main>
     @include('layouts.footer')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
+
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+    < <!-- Vendor JS Files -->
+        <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+        <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/vendor/chart.js/chart.umd.js"></script>
+        <script src="assets/vendor/echarts/echarts.min.js"></script>
+        <script src="assets/vendor/quill/quill.min.js"></script>
+        <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+        <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+        <script src="assets/vendor/php-email-form/validate.js"></script>
+
+        <!-- Template Main JS File -->
+        <script src="{{ asset('js/main.js') }}"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+        </script>
 
 </body>
 
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,73 +1,74 @@
-  <!-- ======= Sidebar ======= -->
-  <aside id="sidebar" class="sidebar">
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+    <ul class="sidebar-nav" id="sidebar-nav">
+        <li class="nav-item">
+            <a class="nav-link " href="/admin/dashboard">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
 
-      <ul class="sidebar-nav" id="sidebar-nav">
+        <li class="nav-item">
+            <a class="nav-link " href="/admin/dashboard">
+                <i class="bi bi-person"></i>
+                <span>Users</span>
+            </a>
+        </li>
 
-          <li class="nav-item">
-              <a class="nav-link " href="index.html">
-                  <i class="bi bi-grid"></i>
-                  <span>Dashboard</span>
-              </a>
-          </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-pc-display-horizontal"></i><span>Questions</span><i
+                    class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="/admin/questions">
+                        <i class="bi bi-circle-fill"></i><span>List Questions</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/admin/questions/create">
+                        <i class="bi bi-circle-fill"></i><span>Add Questions</span>
+                    </a>
+                </li>
+            </ul>
+        </li><!-- End Components Nav -->
 
-          <li class="nav-item">
-              <a class="nav-link " href="/admin/questions">
-                  <i class="bi bi-grid"></i>
-                  <span>Questions</span>
-              </a>
-          </li>
 
-          <li class="nav-heading">Pages</li>
-
-          <li class="nav-item">
-              <a class="nav-link collapsed" href="users-profile.html">
-                  <i class="bi bi-person"></i>
-                  <span>Profile</span>
-              </a>
-          </li><!-- End Profile Page Nav -->
-
-          <li class="nav-item">
-              <a class="nav-link collapsed" href="pages-faq.html">
-                  <i class="bi bi-question-circle"></i>
-                  <span>F.A.Q</span>
-              </a>
-          </li><!-- End F.A.Q Page Nav -->
-
-          <li class="nav-item">
-              <a class="nav-link collapsed" href="pages-contact.html">
-                  <i class="bi bi-envelope"></i>
-                  <span>Contact</span>
-              </a>
-          </li><!-- End Contact Page Nav -->
-
-          <li class="nav-item">
-              <a class="nav-link collapsed" href="pages-register.html">
-                  <i class="bi bi-card-list"></i>
-                  <span>Register</span>
-              </a>
-          </li><!-- End Register Page Nav -->
-
-          <li class="nav-item">
-              <a class="nav-link collapsed" href="pages-login.html">
-                  <i class="bi bi-box-arrow-in-right"></i>
-                  <span>Login</span>
-              </a>
-          </li><!-- End Login Page Nav -->
-
-          <li class="nav-item">
-              <a class="nav-link collapsed" href="pages-error-404.html">
-                  <i class="bi bi-dash-circle"></i>
-                  <span>Error 404</span>
-              </a>
-          </li><!-- End Error 404 Page Nav -->
-
-          <li class="nav-item">
-              <a class="nav-link collapsed" href="pages-blank.html">
-                  <i class="bi bi-file-earmark"></i>
-                  <span>Blank</span>
-              </a>
-          </li><!-- End Blank Page Nav -->
-
-      </ul>
-
-  </aside><!-- End Sidebar-->
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-tags-fill"></i><span>Mood Configurations</span><i
+                    class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="/admin/mood-configurations">
+                        <i class="bi bi-circle-fill"></i><span>List Mood Config</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/admin/blogcategory/add">
+                        <i class="bi bi-circle-fill"></i><span>Add Mood Config</span>
+                    </a>
+                </li>
+            </ul>
+        </li><!-- End Components Nav -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-book-half"></i></i><span>Blog</span><i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="/admin/blog">
+                        <i class="bi bi-circle-fill"></i><span>Daftar Blog</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/admin/blog/add">
+                        <i class="bi bi-circle-fill"></i><span>Tambah Blog</span>
+                    </a>
+                </li>
+            </ul>
+        </li><!-- End Components Nav -->
+    </ul>
+</aside><!-- End Sidebar-->
